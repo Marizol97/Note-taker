3d2bf6352df3e4e72e9fc7419699a9303aaf4f32
Janet Sanchez <janetmarizolsanchez@gmail.com>
2022-12-03 13:35:48 -0700

    Changing file structure

--- miniature-eureka-main/index.js
+++ miniature-eureka-main/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
 app.get('/helloworld', (req, res) => {
     res.send('hello world')
   })
@@ -10,6 +14,12 @@ app.post('/', (req, res) => {
     res.send('POST request to the homepage')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.get('/api/notes', (req, res) => {
+    res.sendFile('.', options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+  })
